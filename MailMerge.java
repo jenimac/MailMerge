5b8547b8f4e8ea0adac4b2f024ae1a7a569970c7
jenimac <jenimac@ymail.com>
2017-08-21 19:08:01 -0500

    Add files via upload

---
+++ MailMerge.java
@@ -0,0 +1,50 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class MailMerge
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        String name = "";
+        float donation;
+        boolean keepGoing = true;
+        
+        System.out.println("Thank You Letters");
+        System.out.println("");
+        
+        while (keepGoing)
+        {
+            System.out.print("Please enter the donor's name. ");
+            name = keyboard.nextLine();
+            if (name.equals("quit")){
+                keepGoing = false;
+            }        
+            
+            System.out.print("Please enter the donation. ");
+            donation = keyboard.nextFloat();
+                
+                System.out.println("Dear " + name + ",");
+                System.out.println("");
+                System.out.println("Thank your for your donation!  We rely on donors like you to keep our");
+                System.out.println("organization effective, and you came through for us.  Your donation of $" 
+                + donation);
+                System.out.println("will help our efforts to make a difference in the world.");
+                System.out.println("");
+                System.out.println("As you may know, we are a registered non-profit organization, so your");
+                System.out.println("donation is tax deductible.  You may use this letter as a receipt for tax");
+                System.out.println("purposes.");
+                System.out.println("");
+                System.out.println("Thanks you again for your support!");
+                System.out.println("");
+                System.out.println("Sincerely,");
+                System.out.println("");
+                System.out.println("Paula Jones");
+                System.out.println("YourCharity.org");
+            
+        }    
+    }
+}
+    
+        
